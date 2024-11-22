8b670c5c44688ae4cd3e0197241c8cca47459ad3
KANITA VERMA <vermakanita13@gmail.com>
2024-11-23 02:35:41 +0530

    Add files via upload

---
+++ Intersaction.js
@@ -0,0 +1,13 @@
+let a=[10,20,40,90]
+let b=[10,20,40,90,30,50];
+for(let i = 0;i<=a.length;i++){
+    for(let j=0;j<b.length;j++){
+        if(a[i]==b[j]){
+            continue
+
+        }
+        var v=a[i]
+        console.log(v);
+    }
+    console.log(v);
+}
